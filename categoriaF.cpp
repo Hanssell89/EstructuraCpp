0914227982fecbce7dc065e530ed67e07567ff2e
Hanssell89 <hanssell.llanes16143@est.uca.edu.ni>
2023-04-26 08:42:20 -0600

    Se agrego el archivo categoriaF

---
+++ categoriaF.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include "estructura.h"
+#include <string.h>
+
+#define MAX 100
+
+using namespace std;
+
+categoria categorias[MAX];
+int pos=0;
+/*CRUD*/
+/*  C crar
+    R leer
+    U actualizar
+    D eliminar*/
+void agregarCategoria(int id, char nombre[]);
+void mostrarCategoria(int i);
+void mostrarCategorias();
+bool existeCategoria(int id);
+void eliminarCategoria(int id);
+void editarCategoria(int id, char nombre[]);
+
+
+void agregarCategoria(int id, char nombre[]){
+    categorias[pos].idCat = id;
+    strcpy(categorias[pos].nombre, nombre);
+    pos++; 
+}
+
+void mostarCategoria(int i){
+    cout << "ID: "<< categorias[i].idCat << endl;
+    cout << "NOMBRE: "<< categorias[i].nombre << endl;
+}
+
+void mostrarCategorias(){
+    for(int i; i < pos; i++){
+    cout << "ID: "<< categorias[i].idCat << endl;
+    cout << "NOMBRE: "<< categorias[i].nombre << endl;
+    }
+}
+
+bool existeCategoria(int id){
+    bool existe=false;
+    int i = 0;
+    while(i < pos){
+        if(categorias[i].idCat == id){
+            existe=true;
+            break;
+        }
+        i++;
+    }
+    return existe;
+}
+
+void eliminarCategoria(int id){
+    if(existeCategoria(id)){
+
+    }else{
+        cout << "NO EXISTE EL CODIGO INDICADO \n";
+    }
+}
+
+void reordenar(int i){
+    for(int i=0; i < pos; i++){
+        
+    }
+}
--- principal.cpp
+++ principal.cpp
@@ -23,6 +23,32 @@ int main(int argc, char const *argv[])
         cout << "=============================\n";
         
     }
+    cout << "\n\n\n";
+
+    int cantp;
+    cout << "Cuantos producto quiere agregar: ";
+    cin >> cantp;
+
+    for (int i = 0; i < cantp; i++){
+        cout << "Ingrese el ID: ";
+        cin >> productos[i].id;
+        cout << "Ingrese el nombre: ";
+        cin >> productos[i].nombre;
+        cout << "Ingrese la descripcion: ";
+        cin >> productos[i].desc;
+        cout << "Ingrese el precio: ";
+        cin >> productos[i].precio;
+    }
+    cout << "\n\n\n";
+    cout << "Mostrar categorias\n";
+    cout << "=============================\n";
+    for(int i = 0; i < cantp; i++){
+        cout << "ID: " << productos[i].id << endl;
+        cout << "Nombre: " << productos[i].nombre << endl;
+        cout << "Descripcion: " << productos[i].desc << endl;
+        cout << "Precio: " << productos[i].precio << endl;
+        cout << "=============================\n";
+    }
     
     return 0;
 }
